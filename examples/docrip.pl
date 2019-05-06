7caeddea9a73f47693ecdab561691679c3822401
Olli Hollmen <olli.hollmen@gmail.com>
2019-05-06 09:01:14 -0700

    Don't create HTML files by default.

--- examples/docrip.pl
+++ examples/docrip.pl
@@ -9,22 +9,30 @@ use Text::Markdown 'markdown';
 use File::Basename;
 use Data::Dumper;
 my @mods = ("DPUT.pm",);
-my @mods2 = ("CLRunner",  "DataRun",  "MDRipper",  "OpRun", "Retrier", "ToolProbe");
+my @mods2 = ("CLRunner",  "DataRun",  "MDRipper",  "OpRun", "Retrier", "ToolProbe", "RSyncer");
 map({push(@mods, "DPUT/$_.pm");} @mods2);
 #my $fname = "../DPUT/DataRun.pm";
+
+#sub fullmd_create (
+#  my (@files, %opts) = @_;
 my $fullmd = "";
+my $createhtml = 0;
 for (@mods) {
   my $fname = "../$_";
   if (!-f $fname) { die("No file: '$fname' !"); }
   my $md = DPUT::MDRipper->new()->rip($fname);
   $fullmd .= $md;
   #print($md);
-  my $html = markdown($md);
-  #print($html);
-  my $outfname = "./".basename($fname).".html";
-  print(STDERR "Suggested name: '$outfname'\n");
-  file_write($outfname, $html);
+  if (createhtml) {
+    my $html = markdown($md);
+    #print($html);
+    my $outfname = "./".basename($fname).".html";
+    print(STDERR "Suggested name: '$outfname'\n");
+    file_write($outfname, $html);
+  }
 }
+#  return $fullmd;
+#} # sub
 # Write README.md
 my $fullfn = "../README.md";
 print(STDERR "Writing Main Module document: '$fullfn'\n");
